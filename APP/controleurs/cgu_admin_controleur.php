10b10b62f18346759ca55b364ea4d810f9edad58
Loicbidaux <34678703+Loicbidaux@users.noreply.github.com>
2018-01-25 23:21:27 +0100

    MAJ CGU
    Les cgu sont désormais fonctionnels

--- APP/controleurs/cgu_admin_controleur.php
+++ APP/controleurs/cgu_admin_controleur.php
@@ -3,11 +3,11 @@ session_start();
 // on inclut le fichier modèle contenant les appels à la BDD
 
 include 'modele/connexion.php';
-include './controleurs/fonction.php';
+include 'controleurs/fonction.php';
 require('modele/cgu_admin_modele.php');
-$reponse = afficherCGU($bdd);
 if (isset($_POST['Contenu'])) {
   modifierCGU($bdd);
 }
+$reponse = afficherCGU($bdd);
 require('vues/cgu_admin_vue.php');
 ?>
