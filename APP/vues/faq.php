bc44cc570cc0178ed383f00d3280ab564c117a30
pierre-isep <pierre.berger@isep.fr>
2018-01-28 16:30:49 +0100

    Update faq.php

--- APP/vues/faq.php
+++ APP/vues/faq.php
@@ -18,7 +18,8 @@
             <img id="imglogo"src="image/logo-bls.png" id="logo">
         </div>
         <div id="hearthomeentete">
-            <titrelogo>Heart'Home</titrelogo>
+            <div id="hometech"><titrelogo>Hometech</titrelogo></div>
+            <div id="domisependessous">Domisep</div>
         </div>
         <div id="conteneur-connexion">
             <div id="connexion">
@@ -39,11 +40,11 @@
     <div class="faq">
         <ul>
             <li>
-                <a href="#question_1">Comment contater domisep ?</a>
+                <a href="#question_1">Comment contacter domisep ?</a>
             </li>
 
             <li>
-                <a href="#question_2">Comment controler mes capteurs ?</a>
+                <a href="#question_2">Comment contrôler mes capteurs ?</a>
             </li>
 
             <li>
@@ -51,7 +52,7 @@
             </li>
 
             <li>
-                <a href="#question_4">Comment ce désabonner ?</a>
+                <a href="#question_4">Comment se désabonner ?</a>
             </li>
 
             <li>
@@ -67,17 +68,17 @@
         <ul>
             <li>
                 <h3 id="question_1" name="question_1">
-                    Comment contater domisep ?
+                    Comment contacter domisep ?
                 </h3>
                 SAV:0123456789 <br />
                 Service technique:0123456788 <br />
-                mail:kum'home@gmail.com <br />
-                adresse: 13 rue des nuages 75014 PARIS
+                Mail:Hometech@gmail.com <br />
+                Adresse: 13 rue des nuages 75014 PARIS
             </li>
 
             <li>
                 <h3 id="question_2" name="question_2">
-                    Comment controler mes capteurs ?
+                    Comment contrôler mes capteurs ?
                 </h3>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </li>
@@ -91,7 +92,7 @@
 
             <li>
                 <h3 id="question_4" name="question_4">
-                    Comment ce désabonner ?
+                    Comment se désabonner ?
                 </h3>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </li>
@@ -103,7 +104,7 @@
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </li>
         </ul>
-        <form  method="post" action="index.php?cible=accueil&fonction=accueil"><a><button id="bouton_sans_style_accueil" type="submit" >Retour accueil</button></a></form>
+        <form  method="post" action="index.php?cible=accueil&fonction=accueil"><a><button id="bouton_sans_style_accueil" type="submit" >Retour Accueil</button></a></form>
     </div>
 </div>
 </div>
