2aa49d454167b6ee8ee347645198282c40e3ceb3
pierre-isep <pierre.berger@isep.fr>
2018-01-30 07:38:04 +0100

    Few bug fixes

--- APP/vues/addUserAdmin.php
+++ APP/vues/addUserAdmin.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-
     <meta charset="UTF-8">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/footer.css">
@@ -10,13 +9,9 @@
     <link rel="script" href="OHcabouge.js">
     <title>Domisep</title>
 </head>
-
 <?php include("header.php");?>
-
 <h1> Veuillez renseigner les informations du nouvel utilisateur : </h1>
-
 <div id="conteneur-body">
-
     <form action="index.php?cible=menu_utilisateur&fonction=outil_ajout_utilisateur" method="POST" enctype="multipart/form-data">
         <label for="nom"><strong>Nom</strong> :</label> <input type="text" name="nom"><br><br>
         <label for="prenom"><strong>Prénom</strong> :</label> <input type="text" name="prenom"><br><br>
@@ -25,18 +20,15 @@
         <label for="mdp2"><strong>Validez votre Mot de passe</strong> :</label> <input type="password" name="mdp2"><br><br>
         <label for="tel"><strong>Numéro de Téléphone</strong> : </label> <input type="text" name="tel"><br><br>
         <label for="mail"><strong>Email</strong> : </label> <input type="text" name="mail"><br><br>
-
         <label for="typeCompte"><strong>Type d'utilisateur : </strong></label>
         <select name="typeCompte">
-            <option value="Utilisateur secondaire">Utilisateur secondaire</option>
+            <option value="1">Administrateur</option>
+            <option value="2">Utilisateur</option>
+            <option value="3">Technicien</option>
         </select><br><br>
-
         <input type="submit" value="Valider">
     </form>
-
 </div>
-
-
 <?php
 include("footer.php");
 ?>
