554dfa4caf263f7f4b401bcb867210f0ba1e2f04
pierre-isep <pierre.berger@isep.fr>
2018-01-30 07:40:07 +0100

    Few bug fixes

--- APP/vues/addUserTool.php
+++ APP/vues/addUserTool.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
     <html>
 <head>
-
     <meta charset="UTF-8">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/footer.css">
@@ -10,34 +9,26 @@
     <link rel="script" href="OHcabouge.js">
     <title>Domisep</title>
 </head>
-
-
 <?php
-
 $nom=$_POST['nom'];
 $prenom=$_POST['prenom'];
 $login=$_POST['login'];
 $mdp=$_POST['mdp1'];
 $mdp2=$_POST['mdp2'];
-$type=2;
+$type=$_POST['typeCompte'];
 $tel=$_POST['tel'];
 $mail=$_POST['mail'];
-
 if(strlen($login) < 5 || $mdp != $mdp2)
 {
     echo "Vous n'avez pas rempli correctement le formulaire !";
 }
-
-$bdd->exec("INSERT INTO `compte`(`Nom_personne`,`Prenom`,`tel`,`Email`,`login`,`mot_de_passe`,`ID_typeCompte`) values('".$nom."','".$prenom."',".$tel.",'".$mail."','".$login."','".$mdp."',".$type.")");
+$bdd->exec("INSERT INTO compte`(Nom_personne`,`Prenom`,`tel`,`Email`,`login`,`mot_de_passe`,`ID_typeCompte`) values('".$nom."','".$prenom."',".$tel.",'".$mail."','".$login."','".$mdp."',".$type.")");
 include('gestion_utilisateurs.php');
-
 $req = $bdd->prepare('SELECT ID_personne FROM compte WHERE Email = :Email ');
 $req->execute(array(
     'Email' => $mail));
 $resultat = $req->fetch();
-
 $req = $bdd->prepare('INSERT INTO ownerlogment(ID_personnes) VALUES( :ID_personnes)');
-
 $req->execute(array(
     'ID_personnes' => $resultat['ID_personne']
 ));
