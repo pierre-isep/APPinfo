cd1435ec5fa9b750278a32e90ad553a4d861a219
bobhom <35703929+bobhom@users.noreply.github.com>
2018-01-31 23:28:10 +0100

    accueil_administrateur
    quelques modifications

--- APP/vues/accueil_administrateur.php
+++ APP/vues/accueil_administrateur.php
@@ -4,6 +4,7 @@
 
     <meta charset="UTF-8">
     <link rel="stylesheet" href="css/style.css">
+ <link rel="stylesheet" href="http://localhost/APP/css/ton_style_de_base.css">
     <link rel="stylesheet" href="css/footer.css">
     <link rel="stylesheet" href="css/header.css">
     <title>Hometech</title>
@@ -13,7 +14,18 @@
 <?php include("header_admin.php");?>
 
 <div id="section">
+	 <div id="conteneur_gerer_ma_maison" style="width:auto"><div class="titre_ajout" > <h6 style="color:skyblue;margin-bottom:2%;"></h6>
 
+
+
+
+</div>
+<h2><b><a  href="index.php?cible=tableau_de_bord_admin&fonction=gestion"><p >Gestion des utilisateurs</p></a></b></h2>
+<h2><b><a  href="index.php?cible=tableau_de_bord_admin&fonction=panne"><p >Gestion des pannes</p></a></b></h2>
+
+</div>
+<div>
+	</div>
 </div>
 
 
