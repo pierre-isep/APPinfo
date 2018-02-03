982907d1c5d8786995fb286cc4bc2055307abdb7
LauraKguez <35940942+LauraKguez@users.noreply.github.com>
2018-02-03 15:03:54 +0100

    Update cgu_admin_modele.php

--- APP/modele/cgu_admin_modele.php
+++ APP/modele/cgu_admin_modele.php
@@ -1,16 +1,15 @@
 <?php
 function afficherCGU($bdd) {
-  $requete = $bdd->query('SELECT * FROM conditions_generales_utilisation ORDER BY ID DESC LIMIT 1 ');
+  $requete = $bdd->query('SELECT * FROM conditions_generales_utilisation ORDER BY ID DESC LIMIT 1 '); // On sélectionne la dernière version des CGU grâce à l'ID décroissant
   return $requete;
 }
 
 function modifierCGU($bdd) {
-    $requete = $bdd->prepare('DELETE FROM conditions_generales_utilisation ');
-    $requete->execute(array());
-    $requete = $bdd->prepare('INSERT INTO conditions_generales_utilisation(Contenu,Date_de_derniere_modification,Date_de_mise_en_ligne) VALUES(:Contenu,NOW(),:Date_de_mise_en_ligne)');
+    /* $requete = $bdd->prepare('DELETE FROM conditions_generales_utilisation ');
+    $requete->execute(array()); */
+    $requete = $bdd->prepare('INSERT INTO conditions_generales_utilisation(Contenu,Date_de_derniere_modification) VALUES(:Contenu,NOW())');
     $requete->execute(array(
         'Contenu' => $_POST['Contenu'],
-        'Date_de_mise_en_ligne' => date('Y-m-d')
   ));
 }
 ?>
